05bc8baa19f99033d05ee67f7b8a6982ba557cd7
Fazer777 <m.soup2016@yandex.ru>
2023-04-21 14:25:59 +0300

    Add fields of class

--- app/src/main/java/com/example/todolistver2/Database/TableNotes.java
+++ app/src/main/java/com/example/todolistver2/Database/TableNotes.java
@@ -1,4 +1,21 @@
 package com.example.todolistver2.Database;
 
+// Таблица Notes (Заметки)
 public class TableNotes {
+    public static final String TABLE_NAME = "Notes";
+    public static final String ID = "id";
+    public static final String DESCRIPTION = "description";
+    public static final String CATEGORY = "category";
+    public static final String NOTE_DATETIME = "note_dateTime";
+
+    public static final String CREATE_TABLE_NOTES =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+                    ID + " INTEGER PRIMARY KEY," +
+                    DESCRIPTION + " TEXT," +
+                    CATEGORY + " TEXT," +
+                    NOTE_DATETIME + " TEXT)";
+
+    public static final String DELETE_TABLE_NOTES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
 }
