1a03c6c1fdd4e05ed0af5ce0e8646d070605a7ee
Fazer777 <m.soup2016@yandex.ru>
2023-04-28 02:02:57 +0300

    Updated color notes to more faded

--- app/src/main/java/com/example/todolistver2/Models/Category.java
+++ app/src/main/java/com/example/todolistver2/Models/Category.java
@@ -1,11 +1,6 @@
-package com.example.todolistver2.fragments.Notes;
+package com.example.todolistver2.Models;
 
-import android.content.res.Resources;
 import android.graphics.Color;
-
-import androidx.core.content.res.ResourcesCompat;
-
-import com.example.todolistver2.R;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,11 +11,11 @@ public class Category implements Serializable {
     public static List<Category> categories = new ArrayList<Category>(){
         {
             add(new Category("Без категории", Color.parseColor("#FFFFFF")));// Белый
-            add(new Category("Работа", Color.parseColor("#FF9999"))); // Красный
-            add(new Category("Фильмы", Color.parseColor("#818EFF"))); // Синий
-            add(new Category("Путешествия", Color.parseColor("#FFD75E"))); // Желтый
-            add(new Category("Повседневное", Color.parseColor("#95E478"))); // Зеленый
-            add(new Category("Игры", Color.parseColor("#FFAD54")));// Оранженвый
+            add(new Category("Работа", Color.parseColor("#FFBDB3"))); // Красный
+            add(new Category("Фильмы", Color.parseColor("#C2C8FF"))); // Синий
+            add(new Category("Путешествия", Color.parseColor("#FFE8A8"))); // Желтый
+            add(new Category("Повседневное", Color.parseColor("#CAFFB8"))); // Зеленый
+            add(new Category("Игры", Color.parseColor("#FFD79E")));// Оранженвый
 
         }};
 
