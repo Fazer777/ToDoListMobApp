21e63b7808b8ef81d05ef28532aa2952e694a55f
Fazer777 <m.soup2016@yandex.ru>
2023-04-28 02:26:29 +0300

    Added two variable for separation and recording category note in database

--- app/src/main/java/com/example/todolistver2/Database/TableNotes.java
+++ app/src/main/java/com/example/todolistver2/Database/TableNotes.java
@@ -5,15 +5,17 @@ public class TableNotes {
     public static final String TABLE_NAME = "Notes";
     public static final String ID = "id";
     public static final String DESCRIPTION = "description";
-    public static final String CATEGORY = "category";
+    public static final String CATEGORY_NAME = "category";
     public static final String NOTE_DATETIME = "note_dateTime";
+    public static final String COLOR_CATEGORY = "color_category";
 
     public static final String CREATE_TABLE_NOTES =
             "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
                     ID + " INTEGER PRIMARY KEY," +
                     DESCRIPTION + " TEXT," +
-                    CATEGORY + " TEXT," +
-                    NOTE_DATETIME + " TEXT)";
+                    CATEGORY_NAME + " TEXT," +
+                    NOTE_DATETIME + " TEXT," +
+                    COLOR_CATEGORY + " TEXT)";
 
     public static final String DELETE_TABLE_NOTES =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
