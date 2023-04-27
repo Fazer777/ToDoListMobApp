7d8e781d929a49476b9291c54f024363be958fe6
Fazer777 <m.soup2016@yandex.ru>
2023-04-28 02:13:45 +0300

    Added override method onKeyDown for handle click on back button in navigation panel

--- app/src/main/java/com/example/todolistver2/Activity/MainActivity.java
+++ app/src/main/java/com/example/todolistver2/Activity/MainActivity.java
@@ -6,45 +6,65 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import androidx.appcompat.widget.Toolbar;
 
-import com.example.todolistver2.Models.Note;
+import com.example.todolistver2.Database.DbManager;
 import com.example.todolistver2.R;
+import com.example.todolistver2.fragments.Notes.NotesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
     private BottomNavigationView bottomNavBar;
     private NavController navController;
     private Toolbar toolbar;
+    //ISelectedBundle selectedBundle;
 
-    List<Note> notes = new ArrayList<>();
+
+    DbManager dbManager;
+    //NotesFragment notesFragment = new NotesFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initElements();
+        setSupportActionBar(toolbar);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
         assert navHostFragment != null;
         navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(bottomNavBar, navController);
-        setSupportActionBar(toolbar);
         NavigationUI.setupActionBarWithNavController(MainActivity.this, navController);
 
+        //dbManager = new DbManager(MainActivity.this);
+        loadDataFromDatabase();
+
+
+    }
+
+    private void loadDataFromDatabase() {
+//        Toast.makeText(MainActivity.this, "Load data start...", Toast.LENGTH_SHORT).show();
+//        List<Note> noteList = new ArrayList<>();
+//        noteList = dbManager.getAllNotesDatabase();
+//        Collections.reverse(noteList);
+//        bundle = new Bundle();
+//        bundle.putSerializable(Constants.BUNDLE_NOTE_LIST_KEY, new ArrayList<>(noteList));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+//        NotesFragment fragInfo = new NotesFragment();
+//        fragInfo.setArguments(bundle);
+//        navController.navigate(R.id.notesFragment,bundle);
     }
+
 
     private void initElements() {
         bottomNavBar = findViewById(R.id.bottom_nav_menu);
@@ -53,9 +73,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        //Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
         return navController.navigateUp()|| super.onSupportNavigateUp();
     }
+
 
 //    public interface ISelectedBundle{
 //        void onBundleSelect(Bundle bundle);
@@ -64,5 +84,13 @@ public class MainActivity extends AppCompatActivity {
 //    public void setOnBundleSelected(ISelectedBundle selectedBundle){
 //        this.selectedBundle = selectedBundle;
 //    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
 }
