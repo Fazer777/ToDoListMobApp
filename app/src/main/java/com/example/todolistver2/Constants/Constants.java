4f2018640189259a15546a6ae76de39c8a05557f
Fazer777 <m.soup2016@yandex.ru>
2023-04-28 02:06:10 +0300

    Added constants for work with intent

--- app/src/main/java/com/example/todolistver2/Constants/Constants.java
+++ app/src/main/java/com/example/todolistver2/Constants/Constants.java
@@ -1,17 +1,19 @@
 package com.example.todolistver2.Constants;
 
-import android.content.res.Resources;
-import android.graphics.Color;
-
-import com.example.todolistver2.fragments.Notes.Category;
-
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Constants {
 
     public static final DateTimeFormatter format_dd_MM_YYYY_HH_mm_ss = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     public static final DateTimeFormatter format_dd_MM_YYYY = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     public static final DateTimeFormatter timeFormat_HH_mm_ss = DateTimeFormatter.ofPattern("HH:mm:ss");
+    //public static final String BUNDLE_NOTE_LIST_KEY = "NOTES";
+    public static final int NOTE_CREATE_ACTION = 121;
+    public static final int NOTE_EDIT_ACTION = 122;
+
+    public static final int INSERT_POSITION =0;
+    public static final int INTENT_DEFAULT_VALUE = -1;
+    public static final String INTENT_INDEX_KEY = "INDEX";
+    public static final String INTENT_UPDATE_NOTE_KEY = "UPDATE_NOTE";
+    public static final String INTENT_CREATE_NOTE_KEY = "CREATE_NOTE";
 }
