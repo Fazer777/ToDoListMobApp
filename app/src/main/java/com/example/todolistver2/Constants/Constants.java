c24beddf4de1f315b7c8a5e0617cf21424963fe2
Fazer777 <m.soup2016@yandex.ru>
2023-05-03 14:28:43 +0300

    Changed separating char in formats by dot. Moved covert functions from class "Note" to class "Constants"

--- app/src/main/java/com/example/todolistver2/Constants/Constants.java
+++ app/src/main/java/com/example/todolistver2/Constants/Constants.java
@@ -1,19 +1,53 @@
 package com.example.todolistver2.Constants;
 
+import android.util.Log;
+
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Constants {
 
-    public static final DateTimeFormatter format_dd_MM_YYYY_HH_mm_ss = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    public static final DateTimeFormatter format_dd_MM_YYYY = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static final DateTimeFormatter format_dd_MM_YYYY_HH_mm_ss = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+    //public static final DateTimeFormatter format_dd_MM_YYYY = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static final DateTimeFormatter format_dd_MM_YYYY = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final DateTimeFormatter timeFormat_HH_mm_ss = DateTimeFormatter.ofPattern("HH:mm:ss");
-    //public static final String BUNDLE_NOTE_LIST_KEY = "NOTES";
     public static final int NOTE_CREATE_ACTION = 121;
     public static final int NOTE_EDIT_ACTION = 122;
+
+    public static final int TIMER_TASK_CREATE_ACTION = 123;
+    public static final int TIMER_TASK_UPDATE_ACTION = 124;
 
     public static final int INSERT_POSITION =0;
     public static final int INTENT_DEFAULT_VALUE = -1;
     public static final String INTENT_INDEX_KEY = "INDEX";
     public static final String INTENT_UPDATE_NOTE_KEY = "UPDATE_NOTE";
     public static final String INTENT_CREATE_NOTE_KEY = "CREATE_NOTE";
+    public static final String INTENT_CREATE_TIMER_TASK_KEY = "CREATE_TASK";
+    public static final String INTENT_UPDATE_TIMER_TASK_KEY = "UPDATE_TASK";
+
+    public static String convertLocalDateTimeToString(LocalDateTime localDatetime){
+        String dateTimeString="";
+        try{
+            dateTimeString = localDatetime.format(Constants.format_dd_MM_YYYY_HH_mm_ss);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            Log.d("LDTtoStr", ex.getMessage());
+            dateTimeString="";
+        }
+        return dateTimeString;
+    }
+
+    public static LocalDateTime convertStringToLocalDate(String strDateTime){
+        LocalDateTime localDateTime = null;
+        try{
+            localDateTime = LocalDateTime.parse(strDateTime, Constants.format_dd_MM_YYYY_HH_mm_ss);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            Log.d("StrToLDT", ex.getMessage());
+
+        }
+        return localDateTime;
+    }
 }
