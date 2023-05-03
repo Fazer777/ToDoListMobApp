a731c45f4edb932a0f8b49c9d3042685309c2c26
Fazer777 <m.soup2016@yandex.ru>
2023-05-03 14:52:16 +0300

    No major changes

--- app/src/main/java/com/example/todolistver2/fragments/Tasks/TasksFragment.java
+++ app/src/main/java/com/example/todolistver2/fragments/Tasks/TasksFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.todolistver2.R;
 
@@ -17,6 +18,7 @@ public class TasksFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getActivity(), "TasksOnCreate", Toast.LENGTH_SHORT).show();
 
     }
 
