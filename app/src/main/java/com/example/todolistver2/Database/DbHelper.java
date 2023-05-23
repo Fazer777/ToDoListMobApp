de63ef2a10fa9a953a196ffba1e3aa9a8be56b57
Fazer777 <m.soup2016@yandex.ru>
2023-05-23 13:50:10 +0300

    Added new table in onCreate method.

--- app/src/main/java/com/example/todolistver2/Database/DbHelper.java
+++ app/src/main/java/com/example/todolistver2/Database/DbHelper.java
@@ -19,9 +19,11 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TableCategories.CREATE_TABLE_CATEGORIES);
         db.execSQL(TableNotes.CREATE_TABLE_NOTES);
         db.execSQL(TableTimerTasks.CREATE_TABLE_TIMER_TASKS);
         db.execSQL(TableTasks.CREATE_TABLE_TASKS);
+        db.execSQL(TableCategories.INSERT_DEFAULT_CATEGORIES);
     }
 
     @Override
